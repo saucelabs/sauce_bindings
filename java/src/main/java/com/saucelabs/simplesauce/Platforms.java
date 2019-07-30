24e925aa01d59975fb4e3d302e8e8bbd4d02615f
nadvolod <nadvolod@gmail.com>
2019-07-30 14:48:14 -0400

    moved fields to the top of the platforms.java

--- java/src/main/java/com/saucelabs/simplesauce/Platforms.java
+++ java/src/main/java/com/saucelabs/simplesauce/Platforms.java
@@ -2,6 +2,16 @@ package com.saucelabs.simplesauce;
 
 public class Platforms
 {
+	public static final String WINDOWS_10 = "Windows 10";
+	public static final String WINDOWS_8_1 = "Windows 8.1";
+	public static final String WINDOWS_8 = "Windows 8";
+
+	public static final String MAC_OS_MOJAVE = "MacOS 10.14";
+	public static final String MAC_OS_HIGH_SIERRA = "MacOS 10.13";
+	public static final String MAC_OS_SIERRA = "MacOS 10.12";
+	public static final String MAC_OS_EL_CAPITAN = "OS X 10.11";
+	public static final String MAC_OS_YOSEMITE = "OS X 10.10";
+
 	public static class WINDOWS
 	{
 		public static String LATEST = WINDOWS_10;
@@ -18,13 +28,5 @@ public class Platforms
 		public static String LATEST = MAC_OS_MOJAVE;
 	}
 
-	public static final String WINDOWS_10 = "Windows 10";
-	public static final String WINDOWS_8_1 = "Windows 8.1";
-	public static final String WINDOWS_8 = "Windows 8";
 
-	public static final String MAC_OS_MOJAVE = "MacOS 10.14";
-	public static final String MAC_OS_HIGH_SIERRA = "MacOS 10.13";
-	public static final String MAC_OS_SIERRA = "MacOS 10.12";
-	public static final String MAC_OS_EL_CAPITAN = "OS X 10.11";
-	public static final String MAC_OS_YOSEMITE = "OS X 10.10";
 }

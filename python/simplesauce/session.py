f2101608e2e89cef12aa7c51ee861dd32b4dac57
joshin4colours <joshua.m.grant@gmail.com>
2019-08-07 09:48:05 -0400

    Implement Specs in Python (#16)
    Initial work to spec out Python bindings

---
+++ python/simplesauce/session.py
@@ -0,0 +1,85 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.remote.remote_connection import RemoteConnection
+from simplesauce.options import SauceOptions
+
+
+SAUCE_USERNAME = os.getenv('SAUCE_USERNAME', None)
+SAUCE_ACCCESS_KEY = os.getenv('SAUCE_ACCESS_KEY', None)
+
+us_ondemand = 'ondemand.us-west-1.saucelabs.com'
+eu_ondemand = 'ondemand.eu-central-1.saucelabs.com'
+
+US_SAUCE_DC_URL = 'https://{}:{}@{}/wd/hub'.format(SAUCE_USERNAME, SAUCE_ACCCESS_KEY, us_ondemand)
+EU_SAUCE_DC_URL = 'https://{}:{}@{}/wd/hub'.format(SAUCE_USERNAME, SAUCE_ACCCESS_KEY, eu_ondemand)
+
+
+class SauceSession():
+
+    def __init__(self, data_center='us', username=None, access_key=None, options=None):
+
+        # TODO: flesh this out
+        self.options = options if options else SauceOptions()
+
+        self._username = username if username else SAUCE_USERNAME
+        self._access_key = access_key if access_key else SAUCE_ACCCESS_KEY
+        self._data_center = data_center
+
+        if data_center.lower() == 'eu':
+            self.remote_url = EU_SAUCE_DC_URL
+        elif data_center.lower() == 'us':
+            self.remote_url = US_SAUCE_DC_URL
+        else:
+            raise KeyError("Invalid Data Center value, please select from 'us' or 'eu'")
+        
+        self.driver = {}
+
+    @property
+    def username(self):
+        return self._username
+
+    @username.setter
+    def username(self, username):
+        self._username = username
+
+    @property
+    def access_key(self):
+        return self._access_key
+
+    @access_key.setter
+    def access_key(self, access_key):
+        self._access_key = access_key
+
+    @property
+    def data_center(self):
+        return self._data_center
+
+    @data_center.setter
+    def data_center(self, data_center):
+        if data_center.lower() == 'eu':
+            self._data_center = data_center
+            self.remote_url = EU_SAUCE_DC_URL
+        elif data_center.lower() == 'us':
+            self._data_center = data_center
+            self.remote_url = US_SAUCE_DC_URL
+        else:
+            raise KeyError("Invalid Data Center value, please select from 'us' or 'eu'")
+
+    def start(self):
+        if not self._username:
+            raise KeyError("Cannot start session, Sauce Username is not set.")
+        elif not self._access_key:
+            raise KeyError("Cannot start session, Sauce Access Key is not set.")
+
+        caps = self.options
+
+        executor = RemoteConnection(self.remote_url, resolve_ip=False)
+        self.driver = webdriver.Remote(
+            command_executor=executor,
+            desired_capabilities=caps.__dict__,
+            keep_alive=True
+        )
+        return self.driver
+
+    def stop(self):
+        self.driver.quit()
---
+++ python/tests/test_session.py
@@ -0,0 +1,122 @@
+import pytest
+import os
+from simplesauce.options import SauceOptions
+from simplesauce.session import SauceSession
+
+
+class TestInit(object):
+
+    def test_defaults_to_US_West_data_center(self):
+        session = SauceSession()
+
+        assert "us-west-1" in session.remote_url
+
+    def test_overrides_default_value_for_data_center(self):
+        session = SauceSession(data_center='eu')
+
+        assert "eu-central-1" in session.remote_url
+
+    def test_raises_exception_if_data_center_is_invalid(self):
+        with pytest.raises(KeyError):
+            session = SauceSession(data_center='uu')
+
+        with pytest.raises(KeyError):
+            session = SauceSession(data_center='')
+
+    def test_accepts_provided_Options_instance(self):
+        options = SauceOptions()
+
+        session = SauceSession(options=options)
+
+        session.options.browserName == 'chrome'
+        session.options.browserVersion == 'latest'
+        session.options.platformName == 'Windows 10'
+
+    def test_generates_default_Options_instance_if_not_provided(self):
+        session = SauceSession()
+
+        session.options.browserName == 'chrome'
+        session.options.browserVersion == 'latest'
+        session.options.platformName == 'Windows 10'
+
+    def test_uses_username_and_access_key_if_ENV_variables_are_defined(self):
+        session = SauceSession()
+
+        session.username == os.environ['SAUCE_USERNAME']
+        session.access_key == os.environ['SAUCE_ACCESS_KEY']
+
+    def test_accepts_provided_username_and_access_key(self):
+        user = 'alice.smith'
+        access_key = 'abce-defg-hijk'
+
+        session = SauceSession(username=user, access_key=access_key)
+
+        session.username == os.environ['SAUCE_USERNAME']
+        session.access_key == os.environ['SAUCE_ACCESS_KEY']
+
+
+class TestDataCenter(object):
+
+    def test_overrides_default_value_for_data_center(self):
+        session = SauceSession()
+
+        session.data_center = 'eu'
+
+        assert "eu-central-1" in session.remote_url
+
+
+class TestUsername(object):
+
+    def test_accepts_provided_username(self):
+        user = 'bob.smith'
+        session = SauceSession()
+
+        session.username = user
+
+        assert session.username == user
+
+
+class TestAccessKey(object):
+
+    def test_accepts_provided_access_key(self):
+        key = 'abcd-1234-5678'
+        session = SauceSession()
+
+        session.access_key = key
+
+        assert session.access_key == key
+
+
+class TestStart(object):
+
+    def test_creates_a_session_on_Sauce_labs(self):
+        session = SauceSession()
+
+        session.start()
+
+        assert session.driver.session_id
+
+    def test_raises_exception_if_no_username_set(self):
+        session = SauceSession()
+
+        session.username = None
+        
+        with pytest.raises(KeyError):
+            session.start()
+
+    def test_raises_exception_if_no_access_key_set(self):
+        session = SauceSession()
+
+        session.access_key = None
+
+        with pytest.raises(KeyError):
+            session.start()
+
+class TestStop(object):
+
+    def test_it_quits_the_driver(self):
+        session = SauceSession()
+
+        session.start()
+
+        session.stop()

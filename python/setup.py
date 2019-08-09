0d71c91a6838df068727aa35f42fd2053313d915
joshgrant <joshua.m.grant@gmail.com>
2019-08-09 13:45:03 -0400

    updates and other needed info

--- python/setup.py
+++ python/setup.py
@@ -1,36 +1,26 @@
-from distutils.command.install import INSTALL_SCHEMES
-from os.path import abspath, dirname, join
-
-from setuptools import setup
-from setuptools.command.install import install
-
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-setup_args = {
-    'cmdclass': {'install': install},
-    'name': 'simple_sauce',
-    'version': '0.0.1',
-    'license': 'MIT',
-    'description': 'Python bindings of the simple_sauce library https://github.com/saucelabs/simple_sauce',
-    'long_description': open(join(abspath(dirname(__file__)), 'README.md')).read(),
-    'url': 'https://github.com/saucelabs/simple_sauce',
-    'classifiers': ['Intended Audience :: Developers',
-                    'Operating System :: POSIX',
-                    'Operating System :: Microsoft :: Windows',
-                    'Operating System :: MacOS :: MacOS X',
-                    'Topic :: Software Development :: Testing',
-                    'Topic :: Software Development :: Libraries',
-                    'Programming Language :: Python',
-                    'Programming Language :: Python :: 2.7',
-                    'Programming Language :: Python :: 3.4',
-                    'Programming Language :: Python :: 3.5',
-                    'Programming Language :: Python :: 3.6',
-                    'Programming Language :: Python :: 3.7'],
-    'install_requires': ['selenium', 'six', 'python-dateutil'],
-    'package_dir': {'simple_sauce': 'simplesauce'},
-    'packages': ['simplesauce'],
-    'zip_safe': False
-}
-
-setup(**setup_args)
+from distutils.core import setup
+setup(
+  name = 'simplesauce',
+  packages = ['simplesauce'],
+  version = '0.1',
+  license='MIT',
+  description = 'Python bindings of the simple_sauce library',
+  author = 'josh.grant',
+  author_email = 'josh.grant@saucelabs.com',
+  url = 'https://github.com/saucelabs/simple_sauce/',
+  download_url = 'https://github.com/saucelabs/simple_sauce/archive/v_001.tar.gz', # based on GitHub releases
+  keywords = ['Sauce Labs', 'testing', 'selenium'],
+  install_requires=[
+          'selenium',
+          'pytest'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Testing',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3', 
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)

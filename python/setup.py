e48380018661624f64c617abf055da8729f2abd6
joshgrant <joshua.m.grant@gmail.com>
2019-08-09 14:02:50 -0400

    cleaning up

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'simplesauce',
   packages = ['simplesauce'],
-  version = '0.1',
+  version = '0.0.1',
   license='MIT',
   description = 'Python bindings of the simple_sauce library',
   author = 'josh.grant',

a96e5c0df52a5c01c7a6964f0d4e6ebd49447479
joshgrant <joshua.m.grant@gmail.com>
2019-08-14 11:30:21 -0400

    codacy check

--- python/setup.py
+++ python/setup.py
@@ -19,7 +19,7 @@ setup(
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Testing',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3', 
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
   ],

3ced00c313b7991a2200fd6bb80ae6952b0a76a0
nadvolod <nadvolod@gmail.com>
2019-09-18 15:22:26 -0400

    add all other data centers to the DataCenter object

--- dotnet/SimpleSauce/DataCenter.cs
+++ dotnet/SimpleSauce/DataCenter.cs
@@ -7,6 +7,10 @@
         public string Value { get; set; }
 
         public static DataCenter UsWest { get { return new DataCenter("https://ondemand.saucelabs.com/wd/hub"); } }
+
+        public static DataCenter UsEast { get { return new DataCenter("https://ondemand.us-east-1.saucelabs.com/wd/hub"); } }
+
+        public static object EuCental { get { return new DataCenter("https://ondemand.eu-central-1.saucelabs.com/wd/hub"); } }
     }
 }
 

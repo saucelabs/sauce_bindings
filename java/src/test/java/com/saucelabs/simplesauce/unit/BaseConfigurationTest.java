dc80f170e46aab096613395876a347bb82cdd13a
nadvolod <nadvolod@gmail.com>
2019-10-04 15:45:36 -0400

    refactoring MacOsTests to remove duplication related to session instantiation and platform retreival

--- java/src/test/java/com/saucelabs/simplesauce/unit/BaseConfigurationTest.java
+++ java/src/test/java/com/saucelabs/simplesauce/unit/BaseConfigurationTest.java
@@ -1,5 +1,6 @@
 package com.saucelabs.simplesauce.unit;
 
+import com.saucelabs.simplesauce.SauceOptions;
 import com.saucelabs.simplesauce.SauceSession;
 import com.saucelabs.simplesauce.interfaces.EnvironmentManager;
 import com.saucelabs.simplesauce.interfaces.RemoteDriverInterface;
@@ -12,12 +13,14 @@ public class BaseConfigurationTest {
     public SauceSession mockSauceSession;
     RemoteDriverInterface fakeRemoteDriver;
     EnvironmentManager fakeEnvironmentManager;
+    SauceOptions sauceOptions;
 
     @Before
     public void setUp()
     {
         fakeRemoteDriver = mock(RemoteDriverInterface.class);
         fakeEnvironmentManager = mock(EnvironmentManager.class);
+        sauceOptions = new SauceOptions();
         mockSauceSession = new SauceSession(fakeRemoteDriver, fakeEnvironmentManager);
         when(fakeEnvironmentManager.getEnvironmentVariable("SAUCE_USERNAME")).thenReturn("test-name");
         when(fakeEnvironmentManager.getEnvironmentVariable("SAUCE_ACCESS_KEY")).thenReturn("accessKey");
--- java/src/test/java/com/saucelabs/simplesauce/unit/MacOsTests.java
+++ java/src/test/java/com/saucelabs/simplesauce/unit/MacOsTests.java
@@ -13,40 +13,46 @@ import static org.junit.Assert.assertEquals;
 public class MacOsTests extends BaseConfigurationTest{
     @Test
     public void withMacOsMojave_returnsMacOs1014() {
-        SauceOptions sauceOptions = new SauceOptions();
         sauceOptions.withMacOsMojave();
-        mockSauceSession = new SauceSession(sauceOptions, fakeRemoteDriver, fakeEnvironmentManager);
+        mockSauceSession = instantiateSauceSession();
 
         mockSauceSession.start();
-        String actualOsThatWasSet = mockSauceSession.sauceSessionCapabilities.getPlatform().toString();
+        String actualOsThatWasSet = getSessionPlatformString();
         assertEquals("macOS 10.14", actualOsThatWasSet);
     }
+    private SauceSession instantiateSauceSession() {
+        return new SauceSession(sauceOptions, fakeRemoteDriver, fakeEnvironmentManager);
+    }
+    private String getSessionPlatformString() {
+        return mockSauceSession.sauceSessionCapabilities.getPlatform().toString();
+    }
+
     @Test
     public void withMacOsHighSierra_returnsMacOs1013() {
         mockSauceSession.withMacOsHighSierra();
         mockSauceSession.start();
-        String actualOsThatWasSet = mockSauceSession.sauceSessionCapabilities.getPlatform().toString();
+        String actualOsThatWasSet = getSessionPlatformString();
         assertEquals("macOS 10.13", actualOsThatWasSet);
     }
     @Test
     public void withMacOsSierra_returnsMacOs1012() {
         mockSauceSession.withMacOsSierra();
         mockSauceSession.start();
-        String actualOsThatWasSet = mockSauceSession.sauceSessionCapabilities.getPlatform().toString();
+        String actualOsThatWasSet = getSessionPlatformString();
         assertEquals("macOS 10.12", actualOsThatWasSet);
     }
     @Test
     public void withMacOsElCapitan_returnsMacOs1011() {
         mockSauceSession.withMacOsXElCapitan();
         mockSauceSession.start();
-        String actualOsThatWasSet = mockSauceSession.sauceSessionCapabilities.getPlatform().toString();
+        String actualOsThatWasSet = getSessionPlatformString();
         assertEquals("OS X 10.11", actualOsThatWasSet);
     }
     @Test
     public void withMacOsYosemite_returnsMacOsX1010() {
         mockSauceSession.withMacOsXYosemite();
         mockSauceSession.start();
-        String actualOsThatWasSet = mockSauceSession.sauceSessionCapabilities.getPlatform().toString();
+        String actualOsThatWasSet = getSessionPlatformString();
         assertEquals("OS X 10.10", actualOsThatWasSet);
     }
     @Test
@@ -68,7 +74,7 @@ public class MacOsTests extends BaseConfigurationTest{
 
         mockSauceSession.start();
 
-        String safariVersionSetThroughSauceSession = mockSauceSession.sauceSessionCapabilities.getPlatform().toString();
+        String safariVersionSetThroughSauceSession = getSessionPlatformString();
         assertThat(Platforms.MAC_OS_MOJAVE, equalToIgnoringCase(safariVersionSetThroughSauceSession));
     }
     @Test

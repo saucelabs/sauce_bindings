8f305dc7d652b434c8673f4d1e4768d1a43fa781
nadvolod <nadvolod@gmail.com>
2019-10-18 11:21:20 +0100

    fix assertions to be assertEquals instead

--- java/src/test/java/com/saucelabs/simplesauce/DataCenterTest.java
+++ java/src/test/java/com/saucelabs/simplesauce/DataCenterTest.java
@@ -1,28 +1,27 @@
 package com.saucelabs.simplesauce;
 
-import org.hamcrest.text.IsEqualIgnoringCase;
 import org.junit.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class DataCenterTest
 {
     @Test
     public void usWestDataCenterUrl_isCorrect()
     {
-        assertThat(DataCenter.USWest,
-                IsEqualIgnoringCase.equalToIgnoringCase("https://ondemand.saucelabs.com/wd/hub"));
+        assertEquals("https://ondemand.saucelabs.com/wd/hub",
+                DataCenter.USWest);
     }
     @Test
     public void usEastDataCenterUrl_isCorrect()
     {
-        assertThat(DataCenter.US_EAST,
-                IsEqualIgnoringCase.equalToIgnoringCase("https://ondemand.us-east-1.saucelabs.com/wd/hub"));
+        assertEquals("https://ondemand.us-east-1.saucelabs.com/wd/hub",
+                DataCenter.US_EAST);
     }
     @Test
     public void euCentralDataCenterUrl_isCorrect()
     {
-        assertThat(DataCenter.EU_CENTRAL,
-                IsEqualIgnoringCase.equalToIgnoringCase("https://ondemand.eu-central-1.saucelabs.com/wd/hub"));
+        assertEquals("https://ondemand.eu-central-1.saucelabs.com/wd/hub",
+                DataCenter.EU_CENTRAL);
     }
 }

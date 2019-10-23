54f014120ac1b6dfd59c5fc65e5ebd65d30db90c
Jaeheon Shim <35268332+jaeheonshim@users.noreply.github.com>
2019-10-23 16:49:25 -0400

    PR Quality fix and update

--- java/src/test/java/com/saucelabs/simplesauce/unit/EdgeTests.java
+++ java/src/test/java/com/saucelabs/simplesauce/unit/EdgeTests.java
@@ -1,10 +1,8 @@
 package com.saucelabs.simplesauce.unit;
 
 import com.saucelabs.simplesauce.EdgeVersion;
-import com.saucelabs.simplesauce.SauceSession;
 import org.junit.Test;
 
-import java.net.MalformedURLException;
 import static org.junit.Assert.assertEquals;
 
 public class EdgeTests extends BaseConfigurationTest{

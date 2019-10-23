30c28afcb54f222f0f621c46c61491d769b1a2e3
nadvolod <nadvolod@gmail.com>
2019-10-23 13:44:05 -0400

    fix compilation error

--- java/src/test/java/com/saucelabs/simplesauce/acceptance/SauceSessionAcceptanceTest.java
+++ java/src/test/java/com/saucelabs/simplesauce/acceptance/SauceSessionAcceptanceTest.java
@@ -4,6 +4,7 @@ import com.saucelabs.simplesauce.ConcreteSystemManager;
 import com.saucelabs.simplesauce.SauceOptions;
 import com.saucelabs.simplesauce.SauceSession;
 import org.junit.After;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;

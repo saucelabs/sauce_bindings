e90e89daca78c9bbf019e691543324d7684b6951
nadvolod <nadvolod@gmail.com>
2019-10-29 13:45:37 -0400

    fix mac os tests

--- java/src/main/java/com/saucelabs/simplesauce/Platforms.java
+++ java/src/main/java/com/saucelabs/simplesauce/Platforms.java
@@ -1,17 +1,17 @@
 package com.saucelabs.simplesauce;
 
-class Platforms
+public class Platforms
 {
 	private static final String WINDOWS_10 = "Windows 10";
 	public static final String WINDOWS_8_1 = "Windows 8.1";
 	public static final String WINDOWS_8 = "Windows 8";
 
-	public static final String MAC_OS_MOJAVE = "MacOS 10.14";
-	private static final String MAC_OS_HIGH_SIERRA = "MacOS 10.13";
-	private static final String MAC_OS_SIERRA = "MacOS 10.12";
+	public static final String MAC_OS_MOJAVE = "macOS 10.14";
+	public static final String MAC_OS_HIGH_SIERRA = "macOS 10.13";
+	private static final String MAC_OS_SIERRA = "macOS 10.12";
 	@SuppressWarnings("SpellCheckingInspection")
-	private static final String MAC_OS_EL_CAPITAN = "OS X 10.11";
-	private static final String MAC_OS_YOSEMITE = "OS X 10.10";
+	public static final String MAC_OS_EL_CAPITAN = "OS X 10.11";
+	public static final String MAC_OS_YOSEMITE = "OS X 10.10";
 
 	private static class WINDOWS
 	{
@@ -19,7 +19,7 @@ class Platforms
 	}
 
 	@SuppressWarnings("SpellCheckingInspection")
-	private static class MAC_OS
+	public static class MAC_OS
 	{
 		public static String MOJAVE = MAC_OS_MOJAVE;
 		public static String HIGH_SIERRA = MAC_OS_HIGH_SIERRA;
@@ -29,6 +29,4 @@ class Platforms
 
 		public static String LATEST = MAC_OS_MOJAVE;
 	}
-
-
 }

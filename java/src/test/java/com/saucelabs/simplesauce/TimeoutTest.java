c4ff882fd162f6f7e9b202ee7a77f7ae79b681fd
nadvolod <nadvolod@gmail.com>
2019-10-29 13:53:51 -0400

    replaced properties with Lombok

--- java/src/test/java/com/saucelabs/simplesauce/TimeoutTest.java
+++ java/src/test/java/com/saucelabs/simplesauce/TimeoutTest.java
@@ -6,45 +6,42 @@ import org.openqa.selenium.MutableCapabilities;
 import static org.junit.Assert.assertEquals;
 
 public class TimeoutTest extends BaseConfigurationTest {
-    //maxDuration timeout should not even be settable. Default is 30 min and we will not
-    //allow our users to set a higher time than that
     @Test
     public void commandTimeout_canBeSet() {
         mockSauceSession.timeouts.setCommandTimeout(100);
         mockSauceSession.start();
-        Object sauceOptions = mockSauceSession.sauceSessionCapabilities.asMap().get("sauce:options");
-        Object commandTimeoutSetInCaps = ((MutableCapabilities) sauceOptions).getCapability("commandTimeout");
-        assertEquals(100, commandTimeoutSetInCaps);
+        assertCorrectCommandSetOnRemoteSession("commandTimeout", 100);
     }
+
+    private void assertCorrectCommandSetOnRemoteSession(String commandTimeout, int expectedTimeout) {
+        Object sauceOptions = mockSauceSession.sauceSessionCapabilities.asMap().get("sauce:options");
+        Object commandTimeoutSetInCaps = ((MutableCapabilities) sauceOptions).getCapability(commandTimeout);
+        assertEquals(expectedTimeout, commandTimeoutSetInCaps);
+    }
+
     @Test
     public void idleTimeout_canBeSet() {
         mockSauceSession.timeouts.setIdleTimeout(100);
         mockSauceSession.start();
-        Object sauceOptions = mockSauceSession.sauceSessionCapabilities.asMap().get("sauce:options");
-        Object commandTimeoutSetInCaps = ((MutableCapabilities) sauceOptions).getCapability("idleTimeout");
-        assertEquals(100, commandTimeoutSetInCaps);
+        assertCorrectCommandSetOnRemoteSession("idleTimeout", 100);
     }
     @Test
     public void maxDuration_canBeSet() throws MaxTestDurationTimeoutExceeded {
         int maxTestDurationInSec = 1800;
-        mockSauceSession.timeouts.setMaxTestDuration(maxTestDurationInSec);
+        mockSauceSession.timeouts.setMaxTestDurationTimeout(maxTestDurationInSec);
         mockSauceSession.start();
-        Object sauceOptions = mockSauceSession.sauceSessionCapabilities.asMap().get("sauce:options");
-        Object commandTimeoutSetInCaps = ((MutableCapabilities) sauceOptions).getCapability("maxDuration");
-        assertEquals(maxTestDurationInSec, commandTimeoutSetInCaps);
+        assertCorrectCommandSetOnRemoteSession("maxDuration", maxTestDurationInSec);
     }
     @Test(expected = MaxTestDurationTimeoutExceeded.class)
     public void maxDuration_setTo1HigherThanMax_throwsException() throws MaxTestDurationTimeoutExceeded {
         int maxTestDurationInSec = 1801;
-        mockSauceSession.timeouts.setMaxTestDuration(maxTestDurationInSec);
+        mockSauceSession.timeouts.setMaxTestDurationTimeout(maxTestDurationInSec);
     }
     @Test()
     public void maxDuration_setTo1LowerThanMax_noException() throws MaxTestDurationTimeoutExceeded {
         int maxTestDurationInSec = 1799;
-        mockSauceSession.timeouts.setMaxTestDuration(maxTestDurationInSec);
+        mockSauceSession.timeouts.setMaxTestDurationTimeout(maxTestDurationInSec);
         mockSauceSession.start();
-        Object sauceOptions = mockSauceSession.sauceSessionCapabilities.asMap().get("sauce:options");
-        Object commandTimeoutSetInCaps = ((MutableCapabilities) sauceOptions).getCapability("maxDuration");
-        assertEquals(maxTestDurationInSec, commandTimeoutSetInCaps);
+        assertCorrectCommandSetOnRemoteSession("maxDuration", maxTestDurationInSec);
     }
 }

7b57e283a9924dea79d0f1fa86deed7cc5ec8b60
nadvolod <nadvolod@gmail.com>
2019-11-06 10:08:21 -0500

    add edge version

---
+++ java/src/main/java/com/saucelabs/simplesauce/enums/EdgeVersion.java
@@ -0,0 +1,16 @@
+package com.saucelabs.simplesauce.enums;
+
+public enum EdgeVersion {
+    E16("16_16299"),
+    E15("15_15063"),
+    E14("14_14393"),
+    E13("13_10586"),
+    E17("17_17134"),
+    E18("18_17763");
+
+    public String label;
+
+    EdgeVersion(String label) {
+        this.label = label;
+    }
+}

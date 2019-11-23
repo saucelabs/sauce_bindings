18802d5e5b08b5df21b206dc5a475bd5f3c45de9
nadvolod <nadvolod@gmail.com>
2019-11-23 15:18:34 -0500

    remove duplication for instatiation of SauceOPtions()

--- dotnet/SimpleSauce.Test/SauceOptionsTests.cs
+++ dotnet/SimpleSauce.Test/SauceOptionsTests.cs
@@ -9,10 +9,14 @@ namespace SimpleSauceTests
     [TestClass]
     public class SauceOptionsTests : BaseTest
     {
+        [TestInitialize]
+        public void Setup()
+        {
+            SauceOptions = new SauceOptions();
+        }
         [TestMethod]
         public void WithEdge_SetsEdgeOptions()
         {
-            SauceOptions = new SauceOptions();
             SauceOptions.WithEdge();
             SauceOptions.EdgeOptions.Should().NotBeNull();
             SauceOptions.EdgeOptions.Should().BeOfType(typeof(EdgeOptions));
@@ -20,7 +24,6 @@ namespace SimpleSauceTests
         [TestMethod]
         public void WithChrome_SetsChromeOptions()
         {
-            SauceOptions = new SauceOptions();
             SauceOptions.WithChrome();
             SauceOptions.SauceChromeOptions.Should().NotBeNull();
             SauceOptions.SauceChromeOptions.Should().BeOfType(typeof(ChromeOptions));
@@ -28,14 +31,12 @@ namespace SimpleSauceTests
         [TestMethod]
         public void WithChrome_DefaultBrowserVersion_Latest()
         {
-            SauceOptions = new SauceOptions();
             SauceOptions.WithChrome();
             SauceOptions.SauceChromeOptions.BrowserVersion.Should().Be("latest");
         }
         [TestMethod]
         public void WithChrome_Platform_Win10()
         {
-            SauceOptions = new SauceOptions();
             SauceOptions.WithChrome();
             SauceOptions.SauceChromeOptions.PlatformName.Should().Be("Windows 10");
         }

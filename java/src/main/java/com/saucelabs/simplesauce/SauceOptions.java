4499341a5ec1ecbd524efb4e557d022bfd127183
Titus Fortner <titusfortner@gmail.com>
2019-12-21 14:56:28 -0600

    [java] this gets the Options class into parity with Ruby/Python

--- java/src/main/java/com/saucelabs/simplesauce/SauceOptions.java
+++ java/src/main/java/com/saucelabs/simplesauce/SauceOptions.java
@@ -3,24 +3,227 @@ package com.saucelabs.simplesauce;
 import com.saucelabs.simplesauce.enums.MacVersion;
 import lombok.Getter;
 import lombok.Setter;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.UnexpectedAlertBehaviour;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Map;
+
 public class SauceOptions {
-    @Getter @Setter private String browserName = BrowserType.CHROME;
-    @Getter @Setter private String browserVersion = "latest";
-    @Getter @Setter private String operatingSystem = Platforms.windowsLatest().getOsVersion();
-    @Getter private ChromeOptions chromeOptions;
+    @Getter
+    @Setter
+    private MutableCapabilities seleniumOptions;
+
+    @Getter
+    @Setter
+    private String browserName = BrowserType.CHROME;
+    @Getter
+    @Setter
+    private String browserVersion = "latest";
+    @Getter
+    @Setter
+    private String platformName = "Windows 10";
+    @Getter
+    @Setter
+    private PageLoadStrategy pageLoadStrategy;
+    @Getter
+    @Setter
+    private Boolean acceptInsecureCerts = null;
+    @Getter
+    @Setter
+    private Proxy proxy;
+    @Getter
+    @Setter
+    private Boolean setWindowRect = null;
+    @Getter
+    @Setter
+    private Map<String, Integer> timeouts;
+    @Getter
+    @Setter
+    private Boolean strictFileInteractability = null;
+    @Getter
+    @Setter
+    private UnexpectedAlertBehaviour unhandledPromptBehavior;
+
+    @Getter
+    @Setter
+    private String accessKey;
+    @Getter
+    @Setter
+    private String avoidProxy;
+    @Setter
+    private String build;
+    @Getter
+    @Setter
+    private String captureHtml;
+    @Getter
+    @Setter
+    private String capturePerformance;
+    @Getter
+    @Setter
+    private String chromedriverVersion;
+    @Getter
+    @Setter
+    private Integer commandTimeout = null;
+    @Getter
+    @Setter
+    private String crmuxdriverVersion;
+    @Getter
+    @Setter
+    private String customData;
+    @Getter
+    @Setter
+    private String disablePopupHandler;
+    @Getter
+    @Setter
+    private String extendedDebugging;
+    @Getter
+    @Setter
+    private String firefoxAdapterVersion;
+    @Getter
+    @Setter
+    private String firefoxProfileUrl;
+    @Getter
+    @Setter
+    private String idleTimeout;
+    @Getter
+    @Setter
+    private String iedriverVersion;
+    @Getter
+    @Setter
+    private Integer maxDuration = null;
+    @Getter
+    @Setter
+    private String name;
+    @Getter
+    @Setter
+    private String parentTunnel;
+    @Getter
+    @Setter
+    private String passed;
+    @Getter
+    @Setter
+    private String prerun;
+    @Getter
+    @Setter
+    private String preventRequeu;
+    @Getter
+    @Setter
+    private String priority;
+    @Getter
+    @Setter
+    private String proxyHost;
+    // TODO - how do we handle this one?
+    @Getter
+    @Setter
+    private String Public;
+    @Getter
+    @Setter
+    private String recordLogs;
+    @Getter
+    @Setter
+    private String recordStreenshots;
+    @Getter
+    @Setter
+    private String recordVideo;
+    @Getter
+    @Setter
+    private String restrictedPublicInfo;
+    @Getter
+    @Setter
+    private String screenResolution;
+    @Getter
+    @Setter
+    private String seleniumVersion;
+    @Getter
+    @Setter
+    private String source;
+    @Getter
+    @Setter
+    private String tags;
+    @Getter
+    @Setter
+    private String timeZone;
+    @Getter
+    @Setter
+    private String tunnelIdentifier;
+    @Getter
+    @Setter
+    private String username;
+    @Getter
+    @Setter
+    private String videoUploadOnPass;
+
+    public static <E extends Enum<E>> boolean isInEnum(String value, Class<E> enumClass) {
+        return Arrays.stream(enumClass.getEnumConstants()).anyMatch(e -> e.name().equals(value));
+    }
+
+    public MutableCapabilities toCapabilities() {
+        MutableCapabilities mutableCapabilities = new MutableCapabilities();
+        MutableCapabilities sauceOptions = new MutableCapabilities();
+        MutableCapabilities sauceCapabilities = new MutableCapabilities();
+
+        Arrays.stream(this.getClass().getDeclaredFields()).forEach(field -> {
+            try {
+                String fieldName = field.getName();
+                if (!fieldName.equals("seleniumOptions")) {
+                    String getter = "get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
+                    Method declaredMethod = SauceOptions.class.getDeclaredMethod(getter);
+                    Object result = declaredMethod.invoke(this);
+                    if (result == null) {
+                        // noop
+                    } else if (isInEnum(fieldName, OptionsEnums.w3c.class)) {
+                        mutableCapabilities.setCapability(fieldName, result);
+                    } else if (isInEnum(fieldName, OptionsEnums.sauce.class)) {
+                        sauceOptions.setCapability(fieldName, result);
+                    }
+                }
+            } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        });
+
+        sauceCapabilities.setCapability("sauce:options", sauceOptions);
+        mutableCapabilities.merge(sauceCapabilities);
+        mutableCapabilities.merge(getSeleniumOptions());
+
+        return mutableCapabilities;
+    }
+
+    public String getBuild() {
+        // Jenkins
+        if (System.getenv("BUILD_TAG") != null) {
+            return System.getenv("BUILD_NAME") + ": " + System.getenv("BUILD_NUMBER");
+            // Bamboo
+        } else if (System.getenv("bamboo_agentId") != null) {
+            return System.getenv("bamboo_shortJobName") + ": " + System.getenv("bamboo_buildNumber");
+            // Travis
+        } else if (System.getenv("TRAVIS_JOB_ID") != null) {
+            return System.getenv("TRAVIS_REPO_SLUG") + ": " + System.getenv("TRAVIS_JOB_NUMBER");
+            // CircleCI
+        } else if (System.getenv("CIRCLE_JOB") != null) {
+            return System.getenv("CIRCLE_JOB") + ": " + System.getenv("CIRCLE_BUILD_NUM");
+            // Gitlab
+        } else if (System.getenv("CI") != null) {
+            return System.getenv("CI_JOB_NAME") + ": " + System.getenv("CI_JOB_ID");
+            // Default
+        } else {
+            return "Build Time: " + Long.toString(System.currentTimeMillis());
+        }
+    }
 
     public SauceOptions withChrome() {
-        chromeOptions = new ChromeOptions();
-        //TODO no longer needed with Chrome 75+
-        chromeOptions.setExperimentalOption("w3c", true);
         browserName = BrowserType.CHROME;
         return this;
     }
-    public SauceOptions withSafari()
-    {
+
+    public SauceOptions withSafari() {
         return withMac(MacVersion.Mojave);
     }
 
@@ -32,32 +235,36 @@ public class SauceOptions {
 
     public SauceOptions withSafari(final String version) {
         String _version = version;
-        if (_version.isEmpty()) { _version = "latest"; }
+        if (_version.isEmpty()) {
+            _version = "latest";
+        }
         browserName = BrowserType.SAFARI;
         browserVersion = _version;
         return this;
     }
 
     public SauceOptions withLinux() {
-        operatingSystem = "Linux";
+        platformName = "Linux";
         return this;
     }
 
     public SauceOptions withWindows10() {
-        operatingSystem = "windows 10";
+        platformName = "windows 10";
         return this;
     }
+
     public SauceOptions withWindows8_1() {
-        operatingSystem = "Windows 8.1";
+        platformName = "Windows 8.1";
         return this;
     }
+
     public SauceOptions withWindows8() {
-        operatingSystem = "Windows 8";
+        platformName = "Windows 8";
         return this;
     }
 
     public SauceOptions withWindows7() {
-        operatingSystem = "Windows 7";
+        platformName = "Windows 7";
         return this;
     }
 
@@ -75,11 +282,13 @@ public class SauceOptions {
         browserVersion = "18.17763";
         return this;
     }
+
     public SauceOptions withEdge17() {
         withEdge();
         browserVersion = "17.17134";
         return this;
     }
+
     public SauceOptions withEdge16() {
         withEdge();
         browserVersion = "16.16299";
@@ -127,7 +336,7 @@ public class SauceOptions {
     }
 
     public SauceOptions withMac(MacVersion macVersion) {
-        operatingSystem = macVersion.label;
+        platformName = macVersion.label;
         browserName = "Safari";
         return this;
     }
--- java/src/test/java/com/saucelabs/simplesauce/SauceOptionsTest.java
+++ java/src/test/java/com/saucelabs/simplesauce/SauceOptionsTest.java
@@ -1,60 +1,132 @@
 package com.saucelabs.simplesauce;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+import static org.openqa.selenium.PageLoadStrategy.EAGER;
+import static org.openqa.selenium.UnexpectedAlertBehaviour.ACCEPT_AND_NOTIFY;
 
 public class SauceOptionsTest {
-    private SauceSession sauceSession;
-    private SauceOptions options;
+    private SauceOptions sauceOptions;
+
+    @Rule
+    public final EnvironmentVariables environmentVariables = new EnvironmentVariables();
 
     @Before
-    public void setUp()
-    {
-        options = new SauceOptions();
+    public void setUp() {
+        environmentVariables.set("BUILD_TAG", " ");
+        environmentVariables.set("BUILD_NAME", "TEMP BUILD");
+        environmentVariables.set("BUILD_NUMBER", "11");
+
+        sauceOptions = new SauceOptions();
     }
 
     @Test
-    public void sauceSession_takesSauceOptions() {
-        sauceSession = new SauceSession(options);
-        assertNotNull(sauceSession);
+    public void usesLatestChromeWindowsVersions() {
+        assertEquals("chrome", sauceOptions.getBrowserName());
+        assertEquals("Windows 10", sauceOptions.getPlatformName());
+        assertEquals("latest", sauceOptions.getBrowserVersion());
     }
 
     @Test
-    public void sauceSession_defaultSauceOptions_returnsChromeBrowser() {
-        SauceRemoteDriver fakeRemoteDriver = mock(SauceRemoteDriver.class);
-        EnvironmentManager fakeEnvironmentManager = mock(EnvironmentManager.class);
-        when(fakeEnvironmentManager.getEnvironmentVariable("SAUCE_USERNAME")).thenReturn("test-name");
-        when(fakeEnvironmentManager.getEnvironmentVariable("SAUCE_ACCESS_KEY")).thenReturn("accessKey");
+    public void acceptsBrowserVersionPlatform() {
+        sauceOptions.setBrowserName("firefox");
+        sauceOptions.setPlatformName("macOS 10.14");
+        sauceOptions.setBrowserVersion("68");
 
-        sauceSession = new SauceSession(options,fakeRemoteDriver, fakeEnvironmentManager);
-        sauceSession.start();
-        String actualBrowser = sauceSession.getCurrentSessionCapabilities().getCapability("browserName").toString();
-        assertEquals("chrome", actualBrowser);
+        assertEquals("firefox", sauceOptions.getBrowserName());
+        assertEquals("macOS 10.14", sauceOptions.getPlatformName());
+        assertEquals("68", sauceOptions.getBrowserVersion());
     }
 
     @Test
-    public void sauceOptions_defaultBrowser_setToChrome() {
-        assertEquals("chrome", options.getBrowserName());
+    public void acceptsW3CSettings() {
+        sauceOptions.setAcceptInsecureCerts(true);
+        sauceOptions.setPageLoadStrategy(EAGER);
+
+        assertEquals(true, sauceOptions.getAcceptInsecureCerts());
+        assertEquals(EAGER, sauceOptions.getPageLoadStrategy());
     }
 
     @Test
-    public void sauceOptions_defaultBrowserVersion_setToLatest() {
-        assertEquals("latest", options.getBrowserVersion());
+    public void acceptsSauceLabsSettings() {
+        sauceOptions.setMaxDuration(1);
+        sauceOptions.setCommandTimeout(2);
+
+        assertEquals(Integer.valueOf(1), sauceOptions.getMaxDuration());
+        assertEquals(Integer.valueOf(2), sauceOptions.getCommandTimeout());
     }
 
     @Test
-    public void sauceOptions_defaultOS_setToWindows() {
-        assertEquals("Windows 10", options.getOperatingSystem());
+    public void acceptsSeleniumSettings() {
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--foo");
+        firefoxOptions.addPreference("foo", "bar");
+        firefoxOptions.setUnhandledPromptBehaviour(ACCEPT_AND_NOTIFY);
+
+        sauceOptions.setSeleniumOptions(firefoxOptions);
+
+        assertEquals(firefoxOptions, sauceOptions.getSeleniumOptions());
     }
 
     @Test
-    public void withChrome_browser_setToChrome() {
-        options.withChrome();
-        assertNotNull(options.getChromeOptions());
+    public void parsesW3CAndSauceAndSeleniumSettings() {
+        // w3c options
+        sauceOptions.setPlatformName("macOS 10.14");
+        sauceOptions.setBrowserVersion("68");
+        sauceOptions.setAcceptInsecureCerts(true);
+        sauceOptions.setPageLoadStrategy(EAGER);
+
+        // sauce options
+        sauceOptions.setMaxDuration(1);
+        sauceOptions.setCommandTimeout(2);
+
+        // Selenium browser options
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--foo");
+        firefoxOptions.addPreference("foo", "bar");
+
+        // Selenium w3c options
+        firefoxOptions.setUnhandledPromptBehaviour(ACCEPT_AND_NOTIFY);
+        sauceOptions.setSeleniumOptions(firefoxOptions);
+
+        Map capabilities = sauceOptions.toCapabilities().toJson();
+
+        // Validate w3c options
+        assertEquals("firefox", capabilities.get("browserName"));
+        assertEquals("macOS 10.14", capabilities.get("platformName"));
+        assertEquals("68", capabilities.get("browserVersion"));
+        assertEquals(EAGER, capabilities.get("pageLoadStrategy"));
+        assertEquals(ACCEPT_AND_NOTIFY, capabilities.get("unhandledPromptBehavior"));
+
+        // Validate Sauce options
+        MutableCapabilities sauceCapabilities = (MutableCapabilities) capabilities.get("sauce:options");
+        assertEquals(1, sauceCapabilities.getCapability("maxDuration"));
+        assertEquals(2, sauceCapabilities.getCapability("commandTimeout"));
+
+        // Validate Selenium options
+        Map firefoxCapabilities = (Map) capabilities.get("moz:firefoxOptions");
+
+        ArrayList<String> expectedArgs = new ArrayList<>();
+        expectedArgs.add("--foo");
+        assertEquals(expectedArgs, firefoxCapabilities.get("args"));
+
+        Map<String, Object> expectedPrefs = new HashMap<>();
+        expectedPrefs.put("foo", "bar");
+        assertEquals(expectedPrefs, firefoxCapabilities.get("prefs"));
+    }
+
+    @Test
+    public void createsDefaultBuildName() {
+        assertEquals("TEMP BUILD: 11", sauceOptions.getBuild());
     }
 }

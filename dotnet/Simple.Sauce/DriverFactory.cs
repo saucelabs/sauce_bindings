0957e134c1d3e54db44526d23ea3809dea55fcce
nadvolod <nadvolod@gmail.com>
2019-12-30 17:33:30 -0500

    clean codacy

--- dotnet/Simple.Sauce/DriverFactory.cs
+++ dotnet/Simple.Sauce/DriverFactory.cs
@@ -6,13 +6,10 @@ namespace Simple.Sauce
 {
     public class DriverFactory : IDriverFactory
     {
-        private IWebDriver _driver;
-
         public IWebDriver CreateRemoteWebDriver(DriverOptions browserOptions)
         {
-            _driver = new RemoteWebDriver(new Uri("https://ondemand.saucelabs.com/wd/hub"),
+            return new RemoteWebDriver(new Uri("https://ondemand.saucelabs.com/wd/hub"),
                 browserOptions.ToCapabilities(), TimeSpan.FromSeconds(600));
-            return _driver;
         }
     }
 }

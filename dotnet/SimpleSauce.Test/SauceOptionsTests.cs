86560f03500468a5b896f0153b0d39309a039237
nadvolod <nadvolod@gmail.com>
2020-01-02 14:40:21 -0500

    [dotnet] Clean SauceOptions tests and add Firefox capabilities

--- dotnet/SimpleSauce.Test/SauceOptionsTests.cs
+++ dotnet/SimpleSauce.Test/SauceOptionsTests.cs
@@ -9,43 +9,5 @@ namespace SimpleSauce.Test
     [TestClass]
     public class SauceOptionsTests : BaseTest
     {
-        [TestInitialize]
-        public void Setup()
-        {
-            SauceOptions = new SauceOptions();
-        }
-        [TestMethod]
-        public void WithEdge_SetsEdgeOptions()
-        {
-            SauceOptions.WithEdge();
-            SauceOptions.ConfiguredEdgeOptions.Should().NotBeNull();
-            SauceOptions.ConfiguredEdgeOptions.Should().BeOfType(typeof(EdgeOptions));
-        }
-        [TestMethod]
-        public void WithChrome_SetsChromeOptions()
-        {
-            SauceOptions.WithChrome();
-            SauceOptions.ConfiguredChromeOptions.Should().NotBeNull();
-            SauceOptions.ConfiguredChromeOptions.Should().BeOfType(typeof(ChromeOptions));
-        }
-        [TestMethod]
-        public void WithChrome_DefaultBrowserVersion_Latest()
-        {
-            SauceOptions.WithChrome();
-            SauceOptions.ConfiguredChromeOptions.BrowserVersion.Should().Be("latest");
-        }
-        [TestMethod]
-        public void WithChrome_DefaultPlatform_Win10()
-        {
-            SauceOptions.WithChrome();
-            SauceOptions.ConfiguredChromeOptions.PlatformName.Should().Be("Windows 10");
-        }
-        [TestMethod]
-        public void WithChrome_VersionChanged_SetsVersion()
-        {
-            SauceOptions.WithChrome("72");
-            SauceOptions.ConfiguredChromeOptions.BrowserVersion.Should().
-                Be("72", "we set a specific chrome version and this version should be passed to ChromeOptions");
-        }
     }
 }

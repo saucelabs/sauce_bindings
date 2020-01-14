088871dba8708a4f3d655d235d9c18dd0f213178
nadvolod <nadvolod@gmail.com>
2020-01-14 13:02:10 -0500

    add Sauce specific SauceOptions options

--- dotnet/Simple.Sauce/Timeout.cs
+++ dotnet/Simple.Sauce/Timeout.cs
@@ -5,5 +5,8 @@
         public int Implicit { get; set; }
         public int PageLoad { get; set; }
         public int Script { get; set; }
+        public int CommandTimeout { get; set; }
+        public int IdleTimeout { get; set; }
+        public int MaxDuration { get; set; }
     }
 }

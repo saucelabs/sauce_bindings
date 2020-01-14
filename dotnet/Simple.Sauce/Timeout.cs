c9c5db51a03e6bf4d52b0b34271fd9d33154223c
nadvolod <nadvolod@gmail.com>
2020-01-14 10:41:31 -0500

    add all w3c caps

---
+++ dotnet/Simple.Sauce/Timeout.cs
@@ -0,0 +1,9 @@
+﻿namespace Simple.Sauce
+{
+    public class Timeout
+    {
+        public int Implicit { get; set; }
+        public int PageLoad { get; set; }
+        public int Script { get; set; }
+    }
+}
--- dotnet/SimpleSauce.Test/SauceOptionsTests.cs
+++ dotnet/SimpleSauce.Test/SauceOptionsTests.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
 using Simple.Sauce;
@@ -32,6 +34,33 @@ namespace SimpleSauce.Test
             SauceOptions.BrowserName.Should().BeEquivalentTo(Browser.Firefox);
             SauceOptions.BrowserVersion.Should().BeEquivalentTo("68");
             SauceOptions.PlatformName.Should().BeEquivalentTo(Platforms.MacOsHighSierra);
+        }
+        [TestMethod]
+        public void AcceptsAllW3CValues()
+        {
+            SauceOptions.PageLoadStrategy = PageLoadStrategy.Eager;
+            SauceOptions.AcceptInsecureCerts = true;
+            SauceOptions.SetWindowRect = true;
+            SauceOptions.Timeout.Implicit = 4;
+            SauceOptions.Timeout.PageLoad = 44;
+            SauceOptions.Timeout.Script = 33;
+
+            var proxy = new Proxy();
+            SauceOptions.Proxy = proxy;
+            SauceOptions.StrictFileInteractability = true;
+            SauceOptions.UnhandledPromptBehavior = UnhandledPromptBehavior.Dismiss;
+
+            var expectedTimeouts = new Dictionary<string, int> 
+                {{"implicit", 4}, {"pageLoad", 44}, {"script", 33}};
+
+            SauceOptions.AcceptInsecureCerts.Should().BeTrue();
+            SauceOptions.PageLoadStrategy.ToString().Should().BeEquivalentTo("eager");
+            SauceOptions.Proxy.Should().BeEquivalentTo(proxy);
+            SauceOptions.SetWindowRect.Should().BeTrue();
+            //SauceOptions.Timeout.Should().BeEquivalentTo(expectedTimeouts, options =>
+            //    options.ExcludingMissingMembers());
+            SauceOptions.StrictFileInteractability.Should().BeTrue();
+            SauceOptions.UnhandledPromptBehavior.ToString().Should().BeEquivalentTo("dismiss");
         }
     }
 }

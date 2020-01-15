05723825c08dd8772e2c29930e738b0aae5f2e73
nadvolod <nadvolod@gmail.com>
2020-01-15 11:56:19 -0500

    add test to convert Timeouts to dictionary

--- dotnet/Simple.Sauce/Timeout.cs
+++ dotnet/Simple.Sauce/Timeout.cs
@@ -1,12 +1,29 @@
-﻿namespace Simple.Sauce
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Simple.Sauce
 {
     public class Timeout
     {
+        [JsonProperty("implicit")]
         public int Implicit { get; set; }
+        [JsonProperty("pageload")]
         public int PageLoad { get; set; }
+        [JsonProperty("script")]
         public int Script { get; set; }
+        [JsonProperty("commandtimeout")]
         public int CommandTimeout { get; set; }
+        [JsonProperty("idletimeout")]
         public int IdleTimeout { get; set; }
+        [JsonProperty("maxduration")]
         public int MaxDuration { get; set; }
+
+        public Dictionary<string, int> ToDictionary()
+        {
+            var json = JsonConvert.SerializeObject(this);
+            var dictionary = JsonConvert.DeserializeObject<Dictionary<string, int>>(json);
+            return dictionary;
+        }
     }
 }

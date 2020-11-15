8c53ca40bcfdd56b1c5d15c11bd0c70563e576dc
titusfortner <titusfortner@gmail.com>
2020-11-15 17:53:43 -0600

    [java] move system and capability commands into their own classes

---
+++ java/src/main/java/com/saucelabs/saucebindings/SystemManager.java
@@ -0,0 +1,37 @@
+package com.saucelabs.saucebindings;
+
+public class SystemManager {
+
+    /**
+     * Shortcut for getting the System Property or the Environment Variable
+     * If the no value is found, an exception is thrown with the provided message
+     *
+     * @param key           the name of the property or environment variable
+     * @param errorMessage  the error message if the property or environment variable is not found
+     * @return              the value of the provided field name
+     */
+    public static String get(String key, String errorMessage) {
+        String value = get(key);
+        if (value == null) {
+            throw new SauceEnvironmentVariablesNotSetException(errorMessage);
+        } else {
+            return value;
+        }
+    }
+
+    /**
+     * Shortcut for getting the System Property or the Environment Variable
+     *
+     * @param key           the name of the property or environment variable
+     * @return              the value of the provided field name
+     */
+    public static String get(String key) {
+        if (System.getProperty(key) != null) {
+            return System.getProperty(key);
+        } else if (System.getenv(key) != null) {
+            return System.getenv(key);
+        } else {
+            return null;
+        }
+    }
+}

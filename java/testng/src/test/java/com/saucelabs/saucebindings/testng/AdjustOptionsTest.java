e90a3da851614e77c474a9535f987bdc21342ed7
titusfortner <titusfortner@gmail.com>
2021-06-07 13:35:38 -0500

    [java] implement BaseTest class for TestNG

---
+++ java/testng/src/test/java/com/saucelabs/saucebindings/testng/AdjustOptionsTest.java
@@ -0,0 +1,28 @@
+package com.saucelabs.saucebindings.testng;
+
+import com.saucelabs.saucebindings.JobVisibility;
+import com.saucelabs.saucebindings.options.SauceOptions;
+import org.openqa.selenium.Capabilities;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static org.testng.Assert.assertEquals;
+
+public class AdjustOptionsTest extends SauceBaseTest {
+
+    public SauceOptions createSauceOptions() {
+        return SauceOptions.firefox()
+                .setMaxDuration(Duration.ofMinutes(30))
+                .setJobVisibility(JobVisibility.TEAM)
+                .setBrowserVersion("87.0")
+                .build();
+    }
+
+    @Test
+    public void useCustomOptions() throws AssertionError {
+        Capabilities caps = driver.getCapabilities();
+        assertEquals("firefox", caps.getBrowserName());
+        assertEquals("87.0", caps.getCapability("browserVersion"));
+    }
+}
---
+++ java/testng/src/test/java/com/saucelabs/saucebindings/testng/QuickstartTest.java
@@ -0,0 +1,13 @@
+package com.saucelabs.saucebindings.testng;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class QuickstartTest extends SauceBaseTest {
+    @Test
+    public void useAllDefaults() throws AssertionError {
+        driver.navigate().to("https://www.saucedemo.com");
+        assertEquals(driver.getTitle(), "Swag Labs");
+    }
+}

074b4f8700d6506d12874cc912e5e84f1b48b82d
titusfortner <titusfortner@gmail.com>
2021-07-22 00:33:11 -0500

    [java] add javadocs for performance

--- java/main/src/main/java/com/saucelabs/saucebindings/SauceSession.java
+++ java/main/src/main/java/com/saucelabs/saucebindings/SauceSession.java
@@ -112,7 +112,7 @@ public class SauceSession {
         if (!sauceOptions.sauce().getCapturePerformance()) {
             throw new InvalidSauceOptionsArgumentException("Can not use performance methods without using `setCapturePerformance()`");
         }
-        return new Performance(driver, sauceOptions.sauce().getName());
+        return new Performance(this);
     }
 
     /**

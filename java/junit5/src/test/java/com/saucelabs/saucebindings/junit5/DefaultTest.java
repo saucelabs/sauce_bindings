051e114a1c58b31971226a328a26c249737d5bd1
Diego Molina <diemol@gmail.com>
2024-07-08 22:10:09 +0200

    Disabling test because the feature has not been implemented

--- java/junit5/src/test/java/com/saucelabs/saucebindings/junit5/DefaultTest.java
+++ java/junit5/src/test/java/com/saucelabs/saucebindings/junit5/DefaultTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -20,6 +21,7 @@ public class DefaultTest {
     System.clearProperty("sauce.disabled");
   }
 
+  @Disabled("This test is disabled because this feature is not yet implemented")
   @Test
   public void useDefaults() {
     Assertions.assertDoesNotThrow(

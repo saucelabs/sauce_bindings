e7ac5b700f542cb347e7db2d7b7fd50f0a2e7218
Diego Molina <diemol@gmail.com>
2024-07-10 08:33:39 +0200

    Fixing formatting issues

--- java/testng/src/test/java/com/saucelabs/saucebindings/testng/AddHooksTest.java
+++ java/testng/src/test/java/com/saucelabs/saucebindings/testng/AddHooksTest.java
@@ -1,28 +1,27 @@
 package com.saucelabs.saucebindings.testng;
 
+import java.lang.reflect.Method;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.lang.reflect.Method;
-
 public class AddHooksTest extends SauceBaseTest {
-    Boolean working = null;
+  Boolean working = null;
 
-    @BeforeMethod
-    protected void setup(Method method) {
-        working = true;
-        super.setup(method);
-    }
+  @BeforeMethod
+  protected void setup(Method method) {
+    working = true;
+    super.setup(method);
+  }
 
-    @AfterMethod
-    protected void teardown() {
-        System.out.println("Can do something in teardown before test watcher methods");
-    }
+  @AfterMethod
+  protected void teardown() {
+    System.out.println("Can do something in teardown before test watcher methods");
+  }
 
-    @Test
-    public void useCustomOptions() {
-        Assert.assertTrue(working);
-    }
+  @Test
+  public void useCustomOptions() {
+    Assert.assertTrue(working);
+  }
 }

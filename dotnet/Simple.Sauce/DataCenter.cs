616144e8c2cdd052e3ce3da7a497373067be030b
Dan Slov <dan.slov@saucelabs.com>
2024-07-15 20:39:14 -0700

    chore: updating dotnet CI

--- dotnet/Simple.Sauce/DataCenter.cs
+++ dotnet/Simple.Sauce/DataCenter.cs
@@ -9,9 +9,9 @@
 
         public string Value { get; set; }
 
-        public static DataCenter UsWest => new DataCenter("https://ondemand.saucelabs.com/wd/hub");
+        public static DataCenter UsWest => new DataCenter("https://ondemand.us-west-1.saucelabs.com/wd/hub");
 
-        public static DataCenter UsEast => new DataCenter("https://ondemand.us-east-1.saucelabs.com/wd/hub");
+        public static DataCenter UsEast => new DataCenter("https://ondemand.us-east-4.saucelabs.com/wd/hub");
 
         public static object EuCental => new DataCenter("https://ondemand.eu-central-1.saucelabs.com/wd/hub");
     }

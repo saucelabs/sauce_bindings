31840e98d00aafcb5f0c7144bbc087f1318e0e95
Dan Slov <dan.slov@saucelabs.com>
2024-07-17 16:10:23 -0700

    ci: adding an sc4 tunnel to python tests

---
+++ python/tests/examples/test_sauce_tunnel.py
@@ -0,0 +1,27 @@
+import os
+
+from saucebindings.options import SauceOptions
+from saucebindings.session import SauceSession
+
+
+class TestSauceOptions(object):
+
+    def test_creates_session(self):
+        # 1. Create a SauceOptions instance with Sauce Labs Specific Options
+        tunnel_name = "sauce-bindings-{}".format(os.environ.get('GITHUB_SHA') or "test")
+        sauceOptions = SauceOptions.chrome(extendedDebugging=True,
+                                           idleTimeout=45,
+                                           tunnelName=tunnel_name,
+                                           timeZone='Alaska')
+
+        # 2. Create Session object with SauceOptions object instance
+        session = SauceSession(sauceOptions)
+
+        # 3. Start Session to get the Driver
+        driver = session.start()
+
+        # 4. Use the driver in your tests just like normal
+        driver.get('https://www.saucedemo.com/')
+
+        # 5. Stop the Session with whether the test passed or failed
+        session.stop(True)

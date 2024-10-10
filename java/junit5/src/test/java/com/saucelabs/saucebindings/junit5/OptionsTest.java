0792c17178331088415576806dab52acbca548eb
titusfortner <titus.fortner@gmail.com>
2024-10-09 21:30:12 -0500

    [junit5] get tags from rest client for testing

--- java/junit5/src/test/java/com/saucelabs/saucebindings/junit5/OptionsTest.java
+++ java/junit5/src/test/java/com/saucelabs/saucebindings/junit5/OptionsTest.java
@@ -4,7 +4,14 @@ import com.saucelabs.saucebindings.SaucePlatform;
 import com.saucelabs.saucebindings.SauceSession;
 import com.saucelabs.saucebindings.options.SauceOptions;
 import java.time.Duration;
+import java.util.List;
+
+import com.saucelabs.saucerest.DataCenter;
+import com.saucelabs.saucerest.SauceREST;
+import com.saucelabs.saucerest.api.JobsEndpoint;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.openqa.selenium.WebDriver;
@@ -33,5 +40,17 @@ public class OptionsTest {
           driver.get("https://www.saucedemo.com/");
         },
         "Driver and Session should be available");
+  }
+
+  @SneakyThrows
+  @Test
+  @Tag("one")
+  @Tag("two")
+    public void setsTags(WebDriver driver) {
+    SauceREST rest = new SauceREST(DataCenter.US_WEST);
+    String id =  ((RemoteWebDriver) driver).getSessionId().toString();
+    List<String> tags = rest.getJobsEndpoint().getJobDetails(id).tags;
+    Assertions.assertTrue(tags.contains("one"));
+    Assertions.assertTrue(tags.contains("one"));
   }
 }
